ae561bf9a2c1a8de55279076a39899f3b9719a6b
paradox1212 <31700877+paradox1212@users.noreply.github.com>
2019-01-30 17:30:37 -0500

    first commit

---
+++ src/mylife/Myinfo.java
@@ -0,0 +1,39 @@
+package mylife;
+
+public class Myinfo {
+      String myName;
+      				int myAge;
+      				int myHeight;
+      				int myWeight;
+      				int myPlace;
+      				int MyFavoritefood;
+      				int myLove;
+      				
+      				public void name() {System.out.println("My name is Jephson Mathew");
+      				public void age() {System.out.println("My age is 25");
+      				
+      				public void weight() {System.out.println("My weight is about 142 lbs")
+      					
+      				public void height() {System.out.println("My height is 69inches")
+      				
+      				public void myPlace() {System.out.println("My place is Valley Cottage Rockland New York")
+      				
+      				public void favouritefood() {
+      					         System.out.println("My favourite food is Indian")
+      		        
+      			    public void love () 
+      					         {System.out.println("I love technology and futurism ")
+      				}
+      				}
+      				}
+      					
+      					
+      				}
+      				}
+      					
+      					}
+      				
+      				
+      				
+      				
+}
---
+++ src/mylife/myinfopt.java
@@ -0,0 +1,5 @@
+package mylife;
+
+public class myinfopt {
+
+}
